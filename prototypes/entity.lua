b9eacbda429eabac36a761c29f696a9c6e0006d7
Erkcurley <eric.curley94@gmail.com>
2017-05-16 13:31:50 -0400

    updated to 1.5

--- prototypes/entity.lua
+++ prototypes/entity.lua
@@ -8,12 +8,16 @@ data:extend( {
         max_health = 300,
         corpse = "big-remnants",
         dying_explosion = "explosion",
-        collision_box = { { - 2.4, - 2.4 }, { 2.4, 2.4 } },
-        selection_box = { { - 2.5, - 2.5 }, { 2.5, 2.5 } },
-        module_slots = 2,
-        allowed_effects = { "consumption", "speed", "productivity", "pollution" },
+        collision_box = {{-2.4, -2.4}, {2.4, 2.4}},
+        selection_box = {{-2.5, -2.5}, {2.5, 2.5}},
+        module_specification =
+        {
+          module_slots = 3
+        },
+        scale_entity_info_icon = true,
+        allowed_effects = {"consumption", "speed", "productivity", "pollution"},
         crafting_categories = { "mohole" },
-        crafting_speed = 1,
+        crafting_speed = 2,
         has_backer_name = true,
         light = {intensity = 1, size = 10},
         energy_source =
@@ -24,63 +28,70 @@ data:extend( {
         },
         energy_usage = "10MW",
         ingredient_count = 3,
-        animation =
+        animation = make_4way_animation_from_spritesheet({ layers =
         {
-            north =
-            {
-                filename = "__base__/graphics/entity/oil-refinery/oil-refinery.png",
-                width = 337,
-                height = 255,
-                frame_count = 1,
-                shift = { 2.515625, 0.484375 }
-            },
-            east =
-            {
-                filename = "__base__/graphics/entity/oil-refinery/oil-refinery.png",
-                x = 337,
-                width = 337,
-                height = 255,
-                frame_count = 1,
-                shift = { 2.515625, 0.484375 }
-            },
-            south =
-            {
-                filename = "__base__/graphics/entity/oil-refinery/oil-refinery.png",
-                x = 674,
-                width = 337,
-                height = 255,
-                frame_count = 1,
-                shift = { 2.515625, 0.484375 }
-            },
-            west =
-            {
-                filename = "__base__/graphics/entity/oil-refinery/oil-refinery.png",
-                x = 1011,
-                width = 337,
-                height = 255,
-                frame_count = 1,
-                shift = { 2.515625, 0.484375 }
+          {
+            filename = "__base__/graphics/entity/oil-refinery/oil-refinery.png",
+            width = 337,
+            height = 255,
+            frame_count = 1,
+            shift = {2.515625, 0.484375},
+            hr_version = {
+              filename = "__base__/graphics/entity/oil-refinery/hr-oil-refinery.png",
+              width = 386,
+              height = 430,
+              frame_count = 1,
+              shift = util.by_pixel(0, -7.5),
+              scale = 0.5
             }
-        },
+          },
+          {
+            filename = "__base__/graphics/entity/oil-refinery/oil-refinery-shadow.png",
+            width = 337,
+            height = 213,
+            frame_count = 1,
+            shift = util.by_pixel(82.5, 26.5),
+            draw_as_shadow = true,
+            hr_version = {
+              filename = "__base__/graphics/entity/oil-refinery/hr-oil-refinery-shadow.png",
+              width = 674,
+              height = 426,
+              frame_count = 1,
+              shift = util.by_pixel(82.5, 26.5),
+              draw_as_shadow = true,
+              scale = 0.5
+            }
+          }
+        }}),
         working_visualisations =
         {
+          {
+            north_position = util.by_pixel(34, -65),
+            east_position = util.by_pixel(-52, -61),
+            south_position = util.by_pixel(-59, -82),
+            west_position = util.by_pixel(57, -58),
+            animation =
             {
-                north_position = { 1.03125, - 1.55 },
-                east_position = { - 1.65625, - 1.3 },
-                south_position = { - 1.875, - 2.0 },
-                west_position = { 1.8437, - 1.2 },
-                animation =
-                {
-                    filename = "__base__/graphics/entity/oil-refinery/oil-refinery-fire.png",
-                    frame_count = 29,
-                    width = 16,
-                    height = 35,
-                    scale = 1.5,
-                    shift = { 0, - 0.5625 },
-                    run_mode = "backward"
-                },
-                light = { intensity = 0.4, size = 6 }
-            }
+              filename = "__base__/graphics/entity/oil-refinery/oil-refinery-fire.png",
+              line_length = 10,
+              width = 20,
+              height = 40,
+              frame_count = 60,
+              animation_speed = 0.75,
+              shift = util.by_pixel(0, -14),
+              hr_version = {
+                filename = "__base__/graphics/entity/oil-refinery/hr-oil-refinery-fire.png",
+                line_length = 10,
+                width = 40,
+                height = 81,
+                frame_count = 60,
+                animation_speed = 0.75,
+                scale = 0.5,
+                shift = util.by_pixel(0, -14.25),
+              },
+            },
+            light = {intensity = 0.4, size = 6, color = {r = 1.0, g = 1.0, b = 1.0}}
+          }
         },
         working_sound =
         {
@@ -90,20 +101,38 @@ data:extend( {
         },
         fluid_boxes =
         {
-            {
-                production_type = "input",
-                pipe_covers = pipecoverspictures(),
-                base_area = 10,
-                base_level = -1,
-                pipe_connections = { { type = "input", position = { - 2, 3 } } }
-            },
-            {
-                production_type = "output",
-                pipe_covers = pipecoverspictures(),
-                base_area = 10,
-                base_level = 1,
-                pipe_connections = { { type = "output", position = { - 2, - 3 } } }
-            }
+          {
+            production_type = "input",
+            pipe_covers = pipecoverspictures(),
+            base_area = 10,
+            base_level = -1,
+            pipe_connections = {{ type="input", position = {-1, 3} }}
+          },
+          {
+            production_type = "input",
+            pipe_covers = pipecoverspictures(),
+            base_area = 10,
+            base_level = -1,
+            pipe_connections = {{ type="input", position = {1, 3} }}
+          },
+          {
+            production_type = "output",
+            pipe_covers = pipecoverspictures(),
+            base_level = 1,
+            pipe_connections = {{ position = {-2, -3} }}
+          },
+          {
+            production_type = "output",
+            pipe_covers = pipecoverspictures(),
+            base_level = 1,
+            pipe_connections = {{ position = {0, -3} }}
+          },
+          {
+            production_type = "output",
+            pipe_covers = pipecoverspictures(),
+            base_level = 1,
+            pipe_connections = {{ position = {2, -3} }}
+          }
         },
         pipe_covers = pipecoverspictures()
     },
@@ -128,18 +157,21 @@ data:extend( {
         {
             {
                 production_type = "input",
+                pipe_covers = pipecoverspictures(),
                 base_area = 10,
                 base_level = -1,
                 pipe_connections = {{ type="input", position = {1, -2} }}
             },
             {
                 production_type = "input",
+                pipe_covers = pipecoverspictures(),
                 base_area = 10,
                 base_level = -1,
                 pipe_connections = {{ type="input", position = {-1, -2} }}
             },
             {
                 production_type = "output",
+                pipe_covers = pipecoverspictures(),
                 base_area = 10,
                 base_level = 1,
                 pipe_connections = {{ type="output", position = {0, 2} }}
@@ -165,6 +197,7 @@ data:extend( {
         },
         collision_box = {{-1.2, -1.2}, {1.2, 1.2}},
         selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
+        target_temperature = 500,
         animation =
         {
             north =
@@ -201,7 +234,7 @@ data:extend( {
             },
         },
         crafting_categories = {"crystallizer"},
-        crafting_speed = 1,
+        crafting_speed = 4,
         energy_source =
         {
             type = "electric",
@@ -210,7 +243,11 @@ data:extend( {
         },
         energy_usage = "210kW",
         ingredient_count = 3,
-        module_slots = 2,
+        module_specification =
+        {
+          module_slots = 3
+        },
+        scale_entity_info_icon = true,
         allowed_effects = {"consumption", "speed", "productivity", "pollution"}
     }
 } )
--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -2,14 +2,14 @@ data:extend({
     {
         type = "recipe",
         name = "mohole-mohole",
-        enabled = true,
+        enabled = false,
         ingredients = {
             {"laser-turret", 25},
             {"filter-inserter", 25},
             {"substation", 2},
             {"explosives", 100},
             {"steel-plate", 100},
-            {"alien-artifact", 10}
+            {"electric-mining-drill", 10}
         },
         result = "mohole-mohole"
     },
@@ -17,7 +17,7 @@ data:extend({
         type = "recipe",
         name = "mohole-lava",
         category = "mohole",
-        enabled = true,
+        enabled = false,
         energy_required = 1,
         ingredients =
         {
@@ -25,20 +25,19 @@ data:extend({
         },
         results=
         {
-            {type="fluid", name="mohole-lava", amount=1}
+            {type="fluid", name="mohole-lava", amount=10}
         },
         icon = "__base__/graphics/icons/fluid/heavy-oil.png"
   },
   {
      type = "recipe",
         name = "mohole-crystallizer",
-        enabled = true,
+        enabled = false,
         ingredients = {
             {"assembling-machine-3", 1},
             {"steel-plate", 20},
-            {"small-pump", 2},
-            {"storage-tank", 1},
-            {"alien-artifact", 1}
+            {"heat-pipe", 10},
+            {"storage-tank", 1}
         },
         result = "mohole-crystallizer"
   },
@@ -46,7 +45,7 @@ data:extend({
         type = "recipe",
         name = "mohole-iron-ore",
         category = "crystallizer",
-        enabled = true,
+        enabled = false,
         energy_required = 10,
         subgroup = "raw-resource",
         ingredients =
@@ -65,7 +64,7 @@ data:extend({
         type = "recipe",
         name = "mohole-copper-ore",
         category = "crystallizer",
-        enabled = true,
+        enabled = false,
         energy_required = 10,
         subgroup = "raw-resource",
         ingredients =
@@ -83,18 +82,17 @@ data:extend({
         type = "recipe",
         name = "mohole-stone",
         category = "crystallizer",
-        enabled = true,
+        enabled = false,
         energy_required = 10,
         subgroup = "raw-resource",
         ingredients =
         {
             {type="fluid", name="mohole-lava", amount=1},
-            {type="fluid", name="water", amount=175}
+            {type="fluid", name="water", amount=50}
         },
         results=
         {
-            {type="fluid", name="water", amount=175},
-            {type="item", name="stone", amount=10},
+            {type="item", name="stone", amount=10}
         },
         icon = "__base__/graphics/icons/stone.png"
   }
