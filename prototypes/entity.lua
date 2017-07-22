07896774617ca61b672d4cc3b6658da51e8cb2b5
Erkcurley <eric.curley94@gmail.com>
2017-07-22 11:37:10 -0400

    Changed the mohole graphic

--- prototypes/entity.lua
+++ prototypes/entity.lua
@@ -2,7 +2,7 @@ data:extend( {
     {
         type = "assembling-machine",
         name = "mohole-mohole",
-        icon = "__base__/graphics/icons/oil-refinery.png",
+        icon = "__Mohole__/graphics/icons/mohole_b0.png",
         flags = { "placeable-neutral", "player-creation" },
         minable = { mining_time = 1, result = "mohole-mohole" },
         max_health = 300,
@@ -29,85 +29,23 @@ data:extend( {
         energy_usage = "10MW",
         ingredient_count = 3,
         animation = make_4way_animation_from_spritesheet({ layers =
-        {
-          {
-            filename = "__base__/graphics/entity/oil-refinery/oil-refinery.png",
-            width = 337,
-            height = 255,
-            frame_count = 1,
-            shift = {2.515625, 0.484375},
-            hr_version = {
-              filename = "__base__/graphics/entity/oil-refinery/hr-oil-refinery.png",
-              width = 386,
-              height = 430,
-              frame_count = 1,
-              shift = util.by_pixel(0, -7.5),
-              scale = 0.5
-            },
-            north =
-            {
-                filename = "__Mohole__/graphics/entity/mohole/mohole_b0.png",
-                width = 337,
-                height = 255,
-                frame_count = 1,
-                shift = { 2.515625, 0.484375 }
-            },
-            east =
-            {
-                filename = "__Mohole__/graphics/entity/mohole/mohole_b0.png",
-                width = 337,
-                height = 255,
-                frame_count = 1,
-                shift = { 2.515625, 0.484375 }
-            },
-            south =
-            {
-                filename = "__Mohole__/graphics/entity/mohole/mohole_b0.png",
-                width = 337,
-                height = 255,
-                frame_count = 1,
-                shift = { 2.515625, 0.484375 }
-            },
-            west =
-            {
-                filename = "__Mohole__/graphics/entity/mohole/mohole_b0.png",
-                width = 337,
-                height = 255,
-                frame_count = 1,
-                shift = { 2.515625, 0.484375 }
-            }
-          }
-        }}),
-        working_visualisations =
-        {
-          {
-            north_position = util.by_pixel(34, -65),
-            east_position = util.by_pixel(-52, -61),
-            south_position = util.by_pixel(-59, -82),
-            west_position = util.by_pixel(57, -58),
-            animation =
-            {
-              filename = "__base__/graphics/entity/oil-refinery/oil-refinery-fire.png",
-              line_length = 10,
-              width = 20,
-              height = 40,
-              frame_count = 60,
-              animation_speed = 0.75,
-              shift = util.by_pixel(0, -14),
-              hr_version = {
-                filename = "__base__/graphics/entity/oil-refinery/hr-oil-refinery-fire.png",
-                line_length = 10,
-                width = 40,
-                height = 81,
-                frame_count = 60,
-                animation_speed = 0.75,
-                scale = 0.5,
-                shift = util.by_pixel(0, -14.25),
-              },
-            },
-            light = {intensity = 0.4, size = 6, color = {r = 1.0, g = 1.0, b = 1.0}}
-          }
-        },
+	    {
+	      {
+	        filename = "__Mohole__/graphics/entity/mohole/moholes.png",
+	        width = 337,
+	        height = 255,
+	        frame_count = 1,
+	        shift = {2.515625, 0.484375},
+	        hr_version = {
+	          filename = "__base__/graphics/entity/oil-refinery/hr-oil-refinery.png",
+	          width = 386,
+	          height = 430,
+	          frame_count = 1,
+	          shift = util.by_pixel(0, -7.5),
+	          scale = 0.5
+	        }
+	      }
+	    }}),
         working_sound =
         {
             sound = { filename = "__base__/sound/oil-refinery.ogg" },
