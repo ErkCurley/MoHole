18a52d5be4d72c644f5dcc91f7c9d0c624a98978
Eric Curley <Eric.Curley94@gmail.com>
2021-06-22 16:16:04 -0400

    This might break everything

---
+++ data-updates.lua
@@ -0,0 +1,128 @@
+if not angelsrefining then
+    if data.raw.item["angels-ore1"] then
+        data.raw.recipe["angels-ore1"] =
+          {
+            type = "recipe",
+            name = "angels-ore1",
+            category = "crystallizer",
+            enabled = true,
+            energy_required = 10,
+            subgroup = "raw-resource",
+            ingredients =
+            {
+                {type="fluid", name="mohole-lava", amount=1},
+            },
+            results=
+            {
+                {type="item", name="angels-ore1", amount=2},
+                {type="item", name="stone", amount=8},
+            },
+            icon = "__angelsrefining__/graphics/icons/angels-ore1.png"
+          }
+    end
+    if data.raw.item["angels-ore2"] then
+        data.raw.recipe["angels-ore2"] =
+          {
+            type = "recipe",
+            name = "angels-ore2",
+            category = "crystallizer",
+            enabled = true,
+            energy_required = 10,
+            subgroup = "raw-resource",
+            ingredients =
+            {
+                {type="fluid", name="mohole-lava", amount=1},
+            },
+            results=
+            {
+                {type="item", name="angels-ore2", amount=2},
+                {type="item", name="stone", amount=8},
+            },
+            icon = "__angelsrefining__/graphics/icons/angels-ore2.png"
+          }
+    end
+    if data.raw.item["angels-ore3"] then
+        data.raw.recipe["angels-ore3"] =
+          {
+            type = "recipe",
+            name = "angels-ore3",
+            category = "crystallizer",
+            enabled = true,
+            energy_required = 10,
+            subgroup = "raw-resource",
+            ingredients =
+            {
+                {type="fluid", name="mohole-lava", amount=1},
+            },
+            results=
+            {
+                {type="item", name="angels-ore3", amount=2},
+                {type="item", name="stone", amount=8},
+            },
+            icon = "__angelsrefining__/graphics/icons/angels-ore3.png"
+          }
+    end
+    if data.raw.item["angels-ore4"] then
+        data.raw.recipe["angels-ore4"] =
+          {
+            type = "recipe",
+            name = "angels-ore4",
+            category = "crystallizer",
+            enabled = true,
+            energy_required = 10,
+            subgroup = "raw-resource",
+            ingredients =
+            {
+                {type="fluid", name="mohole-lava", amount=1},
+            },
+            results=
+            {
+                {type="item", name="angels-ore4", amount=2},
+                {type="item", name="stone", amount=8},
+            },
+            icon = "__angelsrefining__/graphics/icons/angels-ore4.png"
+          }
+    end
+    if data.raw.item["angels-ore5"] then
+        data.raw.recipe["angels-ore5"] =
+          {
+            type = "recipe",
+            name = "angels-ore5",
+            category = "crystallizer",
+            enabled = true,
+            energy_required = 10,
+            subgroup = "raw-resource",
+            ingredients =
+            {
+                {type="fluid", name="mohole-lava", amount=1},
+            },
+            results=
+            {
+                {type="item", name="angels-ore5", amount=2},
+                {type="item", name="stone", amount=8},
+            },
+            icon = "__angelsrefining__/graphics/icons/angels-ore5.png"
+          }
+    end
+    if data.raw.item["angels-ore6"] then
+        data.raw.recipe["angels-ore6"] =
+          {
+            type = "recipe",
+            name = "angels-ore6",
+            category = "crystallizer",
+            enabled = true,
+            energy_required = 10,
+            subgroup = "raw-resource",
+            ingredients =
+            {
+                {type="fluid", name="mohole-lava", amount=1},
+            },
+            results=
+            {
+                {type="item", name="angels-ore6", amount=2},
+                {type="item", name="stone", amount=8},
+            },
+            icon = "__angelsrefining__/graphics/icons/angels-ore6.png"
+          }
+    end
+end
